06cba141edfde32464541bc83fd653b09a96c2c1
prit91 <pncooldude91@gmail.com>
2015-10-16 16:18:39 +0530

    Added the ui for partners view in homescreenviewcontroller

---
+++ Joy/Joy/Views/JOYPartnerTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  JOYPartnerTableViewCell.h
+//  Joy
+//
+//  Created by Pritesh Nandgaonkar on 16/10/15.
+//  Copyright © 2015 Pirates of Powai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOYPartnerTableViewCell : UITableViewCell
+
+@end
