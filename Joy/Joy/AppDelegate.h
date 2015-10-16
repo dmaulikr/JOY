efba93051395a911c791406e9f9ee1756cca0150
Anil Giri <mail.anilgiri@gmail.com>
2015-10-16 12:58:36 +0530

    Added boilerplate project sctructure

---
+++ Joy/Joy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Joy
+//
+//  Created by Anil Giri on 16/10/15.
+//  Copyright © 2015 Pirates of Powai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
