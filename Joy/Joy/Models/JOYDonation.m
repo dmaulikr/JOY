3b98be7e7ee2202f92f3ba7ba3f13721ab02211d
Anil Giri <mail.anilgiri@gmail.com>
2015-10-16 16:15:18 +0530

    Added donation  model

---
+++ Joy/Joy/Models/JOYDonation.m
@@ -0,0 +1,14 @@
+//
+//  JOYDonation.m
+//  Joy
+//
+//  Created by Anil Giri on 16/10/15.
+//  Copyright © 2015 Pirates of Powai. All rights reserved.
+//
+
+#import "JOYDonation.h"
+
+@implementation JOYDonation
+
+@end
+
