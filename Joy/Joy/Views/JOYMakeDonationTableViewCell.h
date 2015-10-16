927d5cb0f38266f8f582d39ba04409a46cbbde93
SANCHIT GOEL <sanchitgoel@SANCHITs-MacBook-Pro.local>
2015-10-16 16:18:36 +0530

    home tab 1st view done

---
+++ Joy/Joy/Views/JOYMakeDonationTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  JOYMakeDonationTableViewCell.h
+//  Joy
+//
+//  Created by SANCHIT GOEL on 16/10/2015.
+//  Copyright © 2015 Pirates of Powai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOYMakeDonationTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIButton *makeDonationButton;
+
+@end
---
+++ Joy/Joy/Views/JOYPreviousDonationsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  JOYPreviousDonationsTableViewCell.m
+//  Joy
+//
+//  Created by SANCHIT GOEL on 16/10/2015.
+//  Copyright © 2015 Pirates of Powai. All rights reserved.
+//
+
+#import "JOYPreviousDonationsTableViewCell.h"
+
+@implementation JOYPreviousDonationsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
