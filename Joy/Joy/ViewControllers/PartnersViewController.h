76da2b0553bda183c3a3d9ac7b61fe1a3be6ee6e
SANCHIT GOEL <sanchitgoel@SANCHITs-MacBook-Pro.local>
2015-10-16 13:43:47 +0530

    3 VCs added

---
+++ Joy/Joy/ViewControllers/PartnersViewController.h
@@ -0,0 +1,13 @@
+//
+//  PartnersViewController.h
+//  Joy
+//
+//  Created by SANCHIT GOEL on 16/10/2015.
+//  Copyright © 2015 Pirates of Powai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartnersViewController : UIViewController
+
+@end
