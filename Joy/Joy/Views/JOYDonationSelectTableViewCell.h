85c00f9661ace510a848bde9766c8811b0ae5670
prit91 <pncooldude91@gmail.com>
2015-10-16 18:49:04 +0530

    Added the view controller for select Donation

---
+++ Joy/Joy/Views/JOYDonationSelectTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  JOYDonationSelectTableViewCell.h
+//  Joy
+//
+//  Created by Pritesh Nandgaonkar on 16/10/15.
+//  Copyright © 2015 Pirates of Powai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOYDonationSelectTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *donationType;
+
+@end
